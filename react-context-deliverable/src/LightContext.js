0c148679304901d4a90a8367dfd9d946aacaf0d3
JustinRobertKing <kingj4@spu.edu>
2019-05-09 10:49:52 -0700

    done

--- react-context-deliverable/src/LightContext.js
+++ react-context-deliverable/src/LightContext.js
@@ -1,9 +1,6 @@
 import React from 'react'
 
-export const colors = ['#000000', '#0000ff', '#00ff00', '#ff00ff', '#ff0000', '#bbbbbb', '#ff69b4'];
-
 export const LightContext = React.createContext({
 	on: false,
-	color: colors[3],
 	toggleSwitch: () => {}
 })
